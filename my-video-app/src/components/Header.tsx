d5b13d5bcd3febf56b789decbae0f3a0b6dda9ec
Babalola Daniel <74297323+BabalolaBrainiac@users.noreply.github.com>
2023-03-02 03:02:01 +0100

    Added header component

--- my-video-app/src/components/Header.tsx
+++ my-video-app/src/components/Header.tsx
@@ -1,15 +1,24 @@
 import React from 'react'
 import Menu from './Menu'
 import {AiOutlineHome} from 'react-icons/ai'
+import { BsFillInfoCircleFill } from "react-icons/bs";
+import Link from 'next/link';
 
 export default function Header() {
   return (
-    <div>
-      <div>
-        <Menu menuTitle="Vidzeo" address="./" Icon={AiOutlineHome} />
+    <div className="flex justify-between mx-2 max-w-6xl sm:mx-auto items-center py-6">
+      <div className="flex">
+        <Menu menuTitle="Home" address="/" Icon={AiOutlineHome} />
+        <Menu menuTitle="About Us" address="/about" Icon={BsFillInfoCircleFill} />
+      </div>
+      <div className='flex items-center space-x-5'>
+        <Link href="/">
+        <h2 className='text-2xl'>
+          <span className='font-bold bg-amber-500 py-1 px-2 rounded-lg mr-1'>Imdb</span>
+          <span className="text-xl hidden sm:inline">Clone</span>
+        </h2>
+        </Link>
       </div>
     </div>
-    
-
   )
 }
--- my-video-app/src/components/Menu.tsx
+++ my-video-app/src/components/Menu.tsx
@@ -5,9 +5,9 @@ export default function Menu(  menuProps: {menuTitle: string,  address: string, 
 const {address, menuTitle, Icon} = menuProps
   return (
     <div>
-        <Link href={address}>  
-        <Icon />
-        <p className='text-red-600'>{menuTitle}</p>
+        <Link href={address} className="mx-4 lg:mx-6 hover:text-amber-600" >   
+        <Icon className="text-2xl sm:hidden mx-4" />
+        <p className="hidden sm:inline my-2 text-sm">{menuTitle}</p>
         </Link>
     </div>
   )
